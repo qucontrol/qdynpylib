d2e350d696254ff63309480a1d53febdf16708f2
Michael Goerz <goerz@physik.uni-kassel.de>
2014-11-01 00:49:46 +0100

    Added read_indexed_matrix routine

--- QDYN/io.py
+++ QDYN/io.py
@@ -61,6 +61,114 @@ def print_2q_gate(U):
         print row_str
 
 
+def read_indexed_matrix(filename, format='coo', shape=None,
+expand_hermitian=True, val_real=False):
+    """
+    Read in a matrix from the file with the given filename
+
+    The file must contain a description in indexed format, like this:
+
+        # row  col  re(val) im(val)
+            0    1  1.0     0.0
+            1    0  0.0     1.0
+
+    The fourth column is optional, if not present, the result will be real.
+
+    Return a matrix in any of the numpy/scipy sparse (or non-sparse) formats.
+    See the documentation of scipy.sparse for information about the different
+    sparse formats
+
+    Arguments
+    ---------
+
+    filename: string
+        Name of file from which to read the matrix
+    format: string, optional
+        Result type:
+        * 'coo' (default): scipy.sparse.coo.coo_matrix
+        * 'array': numpy.ndarray
+        * 'dense': numpy.matrixlib.defmatrix.matrix
+        * 'bsr': scipy.sparse.bsr.bsr_matrix
+        * 'csc': scipy.sparse.csc.csc_matrix
+        * 'csr': scipy.sparse.csr.csr_matrix
+        * 'dia': scipy.sparse.dia.dia_matrix
+        * 'dok': scipy.sparse.dok.dok_matrix
+        * 'lil': scipy.sparse.lil.lil_matrix
+    shape: int or sequence of two ints, optional
+        If given, shape of the resulting matrix. If not given, will be
+        determined from largest occurring index in the data from the input file
+    expand_hermitian: boolean, optional
+        By default, the matrix to be read in is assumed to be Hermitian, and
+        the input file must only contain data for the upper or lower triangle
+        of the Matrix. The other triangle is filled automatically with the
+        complex conjugate values. With `expand_hermitian=False`, the input file
+        must contain *all* entries of the matrix.
+    val_real: boolean, optional
+        If True, only read 3 columns from the input file (i, j, value), even if
+        more columns are present in the file, and return a real matrix.
+    """
+    from scipy.sparse import coo_matrix
+    file_row, file_col \
+    = np.genfromtxt(filename, usecols=(0,1), unpack=True, dtype=np.int)
+    file_real_val \
+    = np.genfromtxt(filename, usecols=(2,), unpack=True, dtype=np.float64)
+    val_is_real = False
+    if not val_real:
+        try:
+            file_imag_val = np.genfromtxt(filename, usecols=(3,), unpack=True,
+                                          dtype=np.float64)
+        except ValueError:
+            # File does not contain a fourth column
+            val_is_real = True
+    # check data consistency, count number of non-zero elements (nnz)
+    nnz = 0
+    upper = None # all vals in upper triangle (True) or lower triangle (False)?
+    for k in xrange(len(file_real_val)):
+        i = file_row[k]
+        j = file_col[k]
+        assert i > 0, "Row-indices in file must be one-based"
+        assert j > 0, "Column-indices in file must be one-based"
+        if i == j:
+            nnz += 1
+        else:
+            if expand_hermitian:
+                if upper is None:
+                    upper = (j > i)
+                assert (j > i) == upper, \
+                "If expand_hermitian is True, file must contain data only " \
+                "for the upper or only for the lower triangle"
+                nnz += 2
+            else:
+                nnz += 1
+    row = np.zeros(nnz, dtype=np.int)
+    col = np.zeros(nnz, dtype=np.int)
+    if val_is_real:
+        val = np.zeros(nnz, dtype=np.float64)
+    else:
+        val = np.zeros(nnz, dtype=np.complex128)
+    l = 0
+    for k in xrange(len(file_real_val)):
+        i = file_row[k] - 1 # adjust for zero-based indexing in Python
+        j = file_col[k] - 1
+        v = file_real_val[k]
+        if not val_is_real:
+            v += 1.0j * file_imag_val[k]
+        row[l] = i
+        col[l] = j
+        val[l] = v
+        l += 1
+        if (i != j) and (expand_hermitian):
+            row[l] = j
+            col[l] = i
+            val[l] = v.conjugate()
+            l += 1
+    m = coo_matrix((val, (row, col)), shape=shape)
+    if format == 'coo':
+        return m
+    else:
+        return getattr(m, 'to'+format)() # e.g. format='dense' -> m.todense()
+
+
 def print_matrix(M, matrix_name=None, limit=1.0e-14, fmt="%9.2E",
     outfile=None):
     """
