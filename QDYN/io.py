e3e3859f54ae5a5e5ddded9b4585d309e99bb2f4
Michael Goerz <goerz@physik.uni-kassel.de>
2014-11-02 21:11:45 +0100

    Added open_file context manager

--- QDYN/io.py
+++ QDYN/io.py
@@ -9,6 +9,48 @@ import scipy.sparse
 from StringIO import StringIO
 
 
+class open_file(object):
+    """
+    Context manager that opens a filename and closes it on exit, but does
+    nothing for file-like objects.
+
+    >>> from StringIO import StringIO
+    >>> file = StringIO('''
+    ... In the world of the very small, where particle and wave
+    ... aspects of reality are equally significant, things do not
+    ... behave in any way that we can understand from our experience
+    ... of the everyday world...all pictures are false, and there is
+    ... no physical analogy we can make to understand what goes on
+    ... inside atoms. Atoms behave like atoms, nothing else.''')
+    >>> with open_file(file) as in_fh: # file could also be a filename
+    ...     for line in in_fh:
+    ...         print line.strip()
+    ...
+    <BLANKLINE>
+    In the world of the very small, where particle and wave
+    aspects of reality are equally significant, things do not
+    behave in any way that we can understand from our experience
+    of the everyday world...all pictures are false, and there is
+    no physical analogy we can make to understand what goes on
+    inside atoms. Atoms behave like atoms, nothing else.
+    """
+    def __init__(self, filename, *args, **kwargs):
+        self.closing = kwargs.pop('closing', False)
+        if isinstance(filename, basestring):
+            self.fh = open(filename, *args, **kwargs)
+            self.closing = True
+        else:
+            self.fh = filename
+
+    def __enter__(self):
+        return self.fh
+
+    def __exit__(self, exc_type, exc_val, exc_tb):
+        if self.closing:
+            self.fh.close()
+        return False
+
+
 def read_U(file, must_be_file=True):
     """ Read in complex 4x4 matrix from file (as written by the QDYN
         write_cmplx_matrix routine). As an alternative to giving the file, you
