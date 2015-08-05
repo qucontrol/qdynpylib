f5094f66d19d91ab12be7b3d819e982e7ce76b83
Michael Goerz <goerz@physik.uni-kassel.de>
2015-08-04 21:43:56 -0400

    Hard dependency on matplotlib in bloch module

--- QDYN/bloch.py
+++ QDYN/bloch.py
@@ -13,25 +13,22 @@ from .linalg import inner
 from numpy import (ndarray, array, linspace, pi, outer, cos, sin, ones,
                    size, sqrt, real, mod, append, ceil, arange, zeros, asarray)
 
-try:
-    import matplotlib.pyplot as plt
-    from matplotlib.patches import FancyArrowPatch
-    from mpl_toolkits.mplot3d import proj3d
+import matplotlib.pyplot as plt
+from matplotlib.patches import FancyArrowPatch
+from mpl_toolkits.mplot3d import proj3d
 
-    class Arrow3D(FancyArrowPatch):
-        def __init__(self, xs, ys, zs, *args, **kwargs):
-            FancyArrowPatch.__init__(self, (0, 0), (0, 0), *args, **kwargs)
+class Arrow3D(FancyArrowPatch):
+    def __init__(self, xs, ys, zs, *args, **kwargs):
+        FancyArrowPatch.__init__(self, (0, 0), (0, 0), *args, **kwargs)
 
-            self._verts3d = xs, ys, zs
+        self._verts3d = xs, ys, zs
 
-        def draw(self, renderer):
-            xs3d, ys3d, zs3d = self._verts3d
-            xs, ys, zs = proj3d.proj_transform(xs3d, ys3d, zs3d, renderer.M)
+    def draw(self, renderer):
+        xs3d, ys3d, zs3d = self._verts3d
+        xs, ys, zs = proj3d.proj_transform(xs3d, ys3d, zs3d, renderer.M)
 
-            self.set_positions((xs[0], ys[0]), (xs[1], ys[1]))
-            FancyArrowPatch.draw(self, renderer)
-except:
-    pass
+        self.set_positions((xs[0], ys[0]), (xs[1], ys[1]))
+        FancyArrowPatch.draw(self, renderer)
 
 Sx = array([[0, 1], [1, 0]])
 Sy = array([[0, -1j], [1j, 0]])
