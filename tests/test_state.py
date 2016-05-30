e6e434115f1d598d62a52fe91a8c7d159f706596
Michael Goerz <goerz@stanford.edu>
2016-05-30 12:39:58 -0700

    Get rid of State class

--- tests/test_state.py
+++ tests/test_state.py
@@ -1,6 +1,7 @@
 from __future__ import print_function, division, absolute_import
 
-import QDYN
+from QDYN.linalg import norm
+from QDYN.state import read_psi_amplitudes, write_psi_amplitudes
 from cmath import sqrt
 import pytest
 import numpy as np
@@ -12,9 +13,9 @@ def test_state_read_write(tmpdir):
       1  1                   0
       3  0.7071067811865476  0.7071067811865476'''
     tmpdir.join('psi.in').write(data_str)
-    state1 = QDYN.state.State.read(str(tmpdir.join('psi.in')), n=3)
-    state1.write(str(tmpdir.join('psi.out')))
-    state2 = QDYN.state.State.read(str(tmpdir.join('psi.out')), n=3)
-    assert (QDYN.linalg.norm(state1.psi - state2.psi) <= 1e-15)
-    state2 = QDYN.state.State(data=np.array([(1/sqrt(2)), 0, (0.5+0.5j)]))
-    assert (QDYN.linalg.norm(state1.psi - state2.psi) <= 1e-15)
+    psi1 = read_psi_amplitudes(str(tmpdir.join('psi.in')), n=3)
+    write_psi_amplitudes(psi1, str(tmpdir.join('psi.out')))
+    psi2 = read_psi_amplitudes(str(tmpdir.join('psi.out')), n=3)
+    assert norm(psi1 - psi2) <= 1e-15
+    psi2 = np.array([(1/sqrt(2)), 0, (0.5+0.5j)])
+    assert norm(psi1 - psi2) <= 1e-15
