63369102322b8912ab0d979b3fc95762270c93fe
Michael Goerz <goerz@stanford.edu>
2018-10-15 19:15:14 -0700

    Drop official support for Python 3.3 and 3.4

--- setup.py
+++ setup.py
@@ -44,8 +44,6 @@ setup(name='QDYN',
           'Natural Language :: English',
           'Topic :: Scientific/Engineering',
           'Programming Language :: Python :: 2.7',
-          'Programming Language :: Python :: 3.3',
-          'Programming Language :: Python :: 3.4',
           'Programming Language :: Python :: 3.5',
           'Programming Language :: Python :: 3.6',
       ]
